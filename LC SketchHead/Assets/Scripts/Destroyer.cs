6a6bdda3fec6d4bab34557d517ada56f2a2e1874
Cassidy <cassidy.lo78@gmail.com>
2022-06-23 20:02:21 -0400

    Doodle Jump Ripoff

---
+++ LC SketchHead/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        //GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+        //Debug.Log(collision.gameObject.name);
+        
+        if (collision.gameObject.name.Contains("Platform"))
+        {
+
+            Destroy(collision.gameObject);
+            collision.gameObject.SetActive(false);
+            Debug.LogWarning("found");
+        } else
+        {
+            GameObject.Find("DoodleHead").SetActive(false);
+        }
+       }
+}
