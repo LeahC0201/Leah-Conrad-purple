6576567b25ffc2fb6d9513d00da5316542fc5ae8
LeahC0201 <leahmelanie999@gmail.com>
2022-07-05 17:02:07 -0500

    LC DontTouchTheCubes

--- LC DontTouchTheCubes/Assets/Scripts/GameControls.cs
+++ LC DontTouchTheCubes/Assets/Scripts/GameControls.cs
@@ -5,10 +5,19 @@ using UnityEngine.UI;
 
 public class GameControls: MonoBehaviour
 {
+    //Timer text object
+    private Text timerText;
+    //Timer counter for adding score
+    private int timerCount;
     // Start is called before the first frame update
     void Start()
     {
-
+        //Game is at a playing state
+        Time.timeScale = 1f;
+        //Executing a courtine
+        StartCoroutine(CountTime());
+        //timerCount text equals finding the score object and using the text component
+        timerText = GameObject.Find("Score").GetComponent<timerText>();
     }
 
     // Update is called once per frame
