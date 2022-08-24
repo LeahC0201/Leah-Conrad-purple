e3f438e6ee6f62a937fd978baf49e0a3dbbef5b2
Cleo <leahmelanie999@gmail.com>
2022-08-24 18:58:36 -0400

    Stack

--- LC Stack/Assets/Scripts/GameController.cs
+++ LC Stack/Assets/Scripts/GameController.cs
@@ -23,6 +23,63 @@ public class GameController : MonoBehaviour
     //Boolean determining if game is over
     [Header("Boolean")]
     public bool Done;
+
+    //IEnumerator X function
+    IEnumerator X()
+    {
+        //Wait three seconds then code is executed
+        yield return new WaitForSeconds(3f);
+        //The scene sample scene is loaded
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    //New block function
+    void Newblock()
+    {
+        if (lastCube != null)
+        {
+            //The current cube position equals to all 3 axis positions to the nearest integer
+            currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x), currentCube.transform.position.y, Mathf.Round(currentCube.transform.position.z));
+            //current cubes size equals to the last cube size
+            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x), lastCube.transform.localScale.y, lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+            //Current cubes positions equals to the current cubes x position last cubes y position z axis position of 0.5
+            currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+            //If the current cube size on x axis is less than or equal to 0 or if the current cube size on the z axis is less than or equal to 0
+
+            if (currentCube.transform.localScale.x <= 0f || currentCube.transform.localScale.z <= 0f)
+            {
+                //Done equals to true
+                Done = true;
+                //Text is visible
+                text.gameObject.SetActive(true);
+                //Text equals to the text of the Final score and which level is played
+                text.text = "Final Score:" + Level;
+                //Start Coroutine function X
+                StartCoroutine(X());
+                //Returns value
+                return;
+
+            }
+
+        }
+        //Last cube equals current cube
+        lastCube = currentCube;
+        //Current cube equals to the spawned last cube
+        currentCube = Instantiate(lastCube);
+        //Current cube's name equals to the level number
+        currentCube.name = Level + " ";
+        //Current cube componenet mesh renderer material; set the color according to the color settings
+        currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
+        //Add 1 to level
+        Level++;
+        Debug.Log(currentCube);
+        Debug.Log(Camera.main);
+        //Camera position equals to the position of the current cube
+        GameObject.Find("Main Camera").transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
+        //Camera looks at the current cube
+        GameObject.Find("Main Camera").transform.LookAt(currentCube.transform.position);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -63,43 +120,5 @@ public class GameController : MonoBehaviour
             //New block function is called
             Newblock();
         }
-    }
-
-    //New block function
-    void Newblock()
-    {
-        if (lastCube != null)
-        {
-            //The current cube position equals to all 3 axis positions to the nearest integer
-            currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x), currentCube.transform.position.y, Mathf.Round(currentCube.transform.position.z));
-            //current cubes size equals to the last cube size
-            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x), lastCube.transform.localScale.y, lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
-            //Current cubes positions equals to the current cubes x position last cubes y position z axis position of 0.5
-            currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
-            //If the current cube size on x axis is less than or equal to 0 or if the current cube size on the z axis is less than or equal to 0
-            if (currentCube.transform.localScale.x <= 0f || currentCube.transform.localScale.z <= 0f)
-            {
-                //Done equals to true
-                Done = true;
-                //Text is visible
-                text.gameObject.SetActive(true);
-                //Text equals to the text of the Final score and which level is played
-                text.text = "Final Score:" + Level;
-                //Start Coroutine function X
-                StartCoroutine(X());
-                //Returns value
-                return;
-
-            }
-        }
-    }
-
-    //IEnumerator X function
-    IEnumerator X()
-    {
-        //Wait three seconds then code is executed
-        yield return new WaitForSeconds(3f);
-        //The scene sample scene is loaded
-        SceneManager.LoadScene("SampleScene");
     }
  }
