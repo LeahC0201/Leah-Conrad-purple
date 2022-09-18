cb1119b8c5d73ebfc061627049c31c255a3ac17a
alison <bunbunsoswag@gmail.com>
2022-09-18 15:22:31 -0400

    MyDroppingBombs2

--- LC MyDroppingBombs2/Assets/Scripts/GameManager.cs
+++ LC MyDroppingBombs2/Assets/Scripts/GameManager.cs
@@ -1,41 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    //??
+    //STEP 58
     private Spawner spawner;
     public GameObject title;
     private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+    public GameObject splash;
+    public GameObject scoreSystem;
+    public Text scoreText;
+    public int pointsWorth = 1;
+    private int score;
 
     void Awake()
     {
-            spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
-            screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
+        scoreText.enabled = false;
     }
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
         title.SetActive(true);
+        splash.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (!gameStarted)
         {
-            spawner.active = true;
-            title.SetActive(false);
-            var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+            if (Input.anyKeyDown)
+            {
+                ResetGame();
+            }
+        }
+        else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
         }
 
         var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
-        foreach(GameObject bombObject in nextBomb){
-            if(bombObject.transform.positiot.y < (-screenBounds.y - 12)){
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if(bombObject.transform.position.y < (-screenBounds.y) - 12 || !gameStarted)
+            {
                 Destroy(bombObject);
             }
         }
+    }
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        splash.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
+
+        scoreText.enabled = true;
+        scoreSystem.GetComponent<Score>().score = 0;
+        scoreSystem.GetComponent<Score>().Start();
+    }
+
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+
+
+        splash.SetActive(true);
     }
 }
