7e7fad35eb76400158de5297fe772d96d23aaab2
Aroosh <leahmelanie999@gmail.com>
2022-10-20 18:47:27 -0400

    Jungle Escape PY

---
+++ BROWN BELT/LC Jungle Escape PY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    bool hit;
+
+    void Update()
+    {
+        hit = Physics.Raycast(transform.position, transform.forward, 4f, 1 << 8);
+        Debug.DrawRay(transform.position, transform.forward, Color.red);
+        if (hit)
+        {
+            Debug.LogWarning("Be Careful!");
+        }
+        else
+        {
+            Debug.Log("All clear!");
+        }
+    }
+}
